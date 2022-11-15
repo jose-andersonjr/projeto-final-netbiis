07eff235501dec6b90f25ae2ec449557004ed209
José Anderson <jose.andersonjr2@gmail.com>
2022-11-15 16:56:51 -0300

    API CEP implementada com sucesso e correção na imagemda página de login

---
+++ registrar.js
@@ -0,0 +1,50 @@
+// =========================== AUTOCOMPLETE CPF ========================================= //
+
+const inputCEP = document.querySelector('#cep')
+const inputCidade = document.querySelector('#cidade')
+const inputBairro = document.querySelector('#bairro')
+const inputLogradouro = document.querySelector('#logradouro')
+
+
+
+// Validar o CEP para permitir apenas números
+inputCEP.addEventListener("keypress", (e) => {
+    const numeros = /[0-9]/;
+    const key = String.fromCharCode(e.keyCode);
+
+    if (!numeros.test(key)) {
+        e.preventDefault();
+        return;
+    }
+})
+
+// Checar se o texto digitado tem o tamanho correto
+inputCEP.addEventListener("keyup", (e) => {
+    const valorInput = e.target.value;
+    
+    if(valorInput.length === 8) {
+        getAddress(valorInput);
+    }
+    
+});
+
+
+// Pegar o endereço do cliente
+const getAddress = async (cep) => {
+
+    const apiURL = `https://viacep.com.br/ws/${cep}/json/`
+
+    const response = await fetch(apiURL)
+
+    const data = await response.json()
+
+    if (data.erro) {
+        alert("CEP inválido, valor insira o CEP corretamente!")
+    }else{
+        inputCidade.value = data.cidade
+        inputBairro.value = data.bairro
+        inputLogradouro.value = data.logradouro
+    }
+};
+
+// ================================================================================ //
